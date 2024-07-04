f8371213802b93d3b0ce93cf9d3ebe5405c37c78
Vario | Web agency <operational.vario@gmail.com>
2024-07-04 15:32:57 +0300

    Update plasmic-init.ts

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,4 +1,5 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+import * as React from 'react';
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
@@ -13,6 +14,29 @@ export const PLASMIC = initPlasmicLoader({
   // project, allowing you to see your designs without publishing.  Please
   // only use this for development, as this is significantly slower.
   preview: false,
+});
+
+export interface HelloWorldProps {
+  children?: React.ReactNode;
+  className?: string;
+  verbose?: boolean;
+}
+
+export function HelloWorld({ children, className, verbose }: HelloWorldProps) {
+  return (
+    <div className={className} style={{ padding: '20px' }}>
+      <p>Hello there! {verbose && 'Really nice to meet you!'}</p>
+      <div>{children}</div>
+    </div>
+  );
+}
+
+PLASMIC.registerComponent(HelloWorld, {
+  name: 'HelloWorld',
+  props: {
+    verbose: 'boolean',
+    children: 'slot'
+  }
 });
 
 // You can register any code components that you want to use here; see
